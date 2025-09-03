04e12e740779902f1849c007cc4373bf95e94273
rafhael.cunha <rafhael.cunha@vacaria.ifrs.edu.br>
2025-09-03 16:06:18 -0300

    exercícios da aula 3

---
+++ aula3/exercicios/ex04/script.js
@@ -0,0 +1,3 @@
+let nome = "João"; // adicionei uma aspas que estava faltando nesta instrução;
+let idade = 30;
+console.log("Nome: ", nome, "Idade: ", idade); // adicionei uma virgula entre a label idade e a variável idade
