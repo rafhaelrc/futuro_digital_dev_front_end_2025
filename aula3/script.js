5ac64b40c802ad7ee652c5d16a4e28134b609227
rafhael.cunha <rafhael.cunha@vacaria.ifrs.edu.br>
2025-09-03 11:01:44 -0300

    Aulas 2 e 3

---
+++ aula3/script.js
@@ -0,0 +1,35 @@
+let a,b,c;
+
+const price1 = 5;
+let price2 = 10;
+let total = price1 + price2;
+price2 = 15;
+console.log(price2);
+
+
+let decision = true;
+console.log(decision);
+let decision2 = false;
+console.log(decision2);
+
+mostrarResultado();
+
+// ABSTRAÇÃO
+
+// comentário em linha
+
+//let x = 10;
+
+/* comentário em bloco */
+/*
+a = 10.1;
+b = 6;
+c = (a + b) * 5;
+console.log(c);
+*/
+
+
+function mostrarResultado(){
+    //document.getElementById("demo").innerHTML = NomeCompleto;
+    console.log("chamei a função...")
+}
